04176e7ce0766fff5959006822029138adbe7951
André Silva <andre06x@gmail.com>
2024-02-12 21:30:55 -0300

    add registers server

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,10 @@
 import fastify from "fastify";
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
-import cookie from "@fastify/cookie";
 import { voteOnPoll } from "./routes/vote-on-poll";
+import fastifyWebsocket from "@fastify/websocket";
+import cookie from "@fastify/cookie";
+import { pollResult } from "./ws/poll-results";
 
 const app = fastify();
 
@@ -11,9 +13,12 @@ app.register(cookie, {
   hook: "onRequest",
   parseOptions: {},
 });
+
+app.register(fastifyWebsocket);
 app.register(createPoll);
 app.register(getPoll);
 app.register(voteOnPoll);
+app.register(pollResult);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server on");
